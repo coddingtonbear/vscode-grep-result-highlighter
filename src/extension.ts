f9fe5c01de63d6811751312bce526f376c905049
Adam Coddington <me@adamcoddington.net>
2018-09-28 13:03:11 -0700

    And so it begins.

---
+++ src/extension.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+import * as vscode from 'vscode';
+
+// this method is called when the extension is activated
+export function activate(context: vscode.ExtensionContext) {
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+    // Nothing to do here
+}
